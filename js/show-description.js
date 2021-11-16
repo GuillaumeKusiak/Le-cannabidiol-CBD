626b7affa7ac48c743026bffdf4a45c359236b02
Guillaume <guillaume.kusiak@etu.univ-amu.fr>
2021-11-16 21:13:58 +0100

    finish all website

--- js/show-description.js
+++ js/show-description.js
@@ -1,39 +1,45 @@
-function showDescriptions()
+function showDescriptionsFirst()
 {
     var allInfosFirst = document.querySelector(".all-infos-first");
-    var allInfosSecond = document.querySelector(".all-infos-second");
-    var allInfosThird = document.querySelector(".all-infos-third");
 
     if (allInfosFirst.style.display === "none")
     {
         allInfosFirst.style.display = "block";
-        document.querySelector(".show-infos span").style.transform = "rotate(180deg)";
+        document.querySelector(".all-infos-first .show-infos span").style.transform = "rotate(180deg)";
     }
     else
     {
         allInfosFirst.style.display = "none";
-        document.querySelector(".show-infos span").style.transform = "rotate(0deg)";
+        document.querySelector(".all-infos-first .show-infos span").style.transform = "rotate(0deg)";
     }
+}
+function showDescriptionsSecond()
+{
+    var allInfosSecond = document.querySelector(".all-infos-second");
 
     if (allInfosSecond.style.display === "none")
     {
         allInfosSecond.style.display = "block";
-        document.querySelector(".show-infos span").style.transform = "rotate(180deg)";
+        document.querySelector(".all-infos-second .show-infos span").style.transform = "rotate(180deg)";
     }
     else
     {
         allInfosSecond.style.display = "none";
-        document.querySelector(".show-infos span").style.transform = "rotate(0deg)";
+        document.querySelector(".all-infos-second .show-infos span").style.transform = "rotate(0deg)";
     }
+}
+function showDescriptionsThird()
+{
+    var allInfosThird = document.querySelector(".all-infos-third");
 
     if (allInfosThird.style.display === "none")
     {
         allInfosThird.style.display = "block";
-        document.querySelector(".show-infos span").style.transform = "rotate(180deg)";
+        document.querySelector(".all-infos-third .show-infos span").style.transform = "rotate(180deg)";
     }
     else
     {
         allInfosThird.style.display = "none";
-        document.querySelector(".show-infos span").style.transform = "rotate(0deg)";
+        document.querySelector(".all-infos-third .show-infos span").style.transform = "rotate(0deg)";
     }
 }
