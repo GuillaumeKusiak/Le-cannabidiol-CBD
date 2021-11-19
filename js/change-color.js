7a60cfc413b019a5ff6a58b0284cce18db06a0b8
Guillaume <guillaume.kusiak@etu.univ-amu.fr>
2021-11-19 21:41:00 +0100

    finish new theme

--- js/change-color.js
+++ js/change-color.js
@@ -1,14 +1,11 @@
 function changeStyle() {
-    console.log("check ok");
     let link = document.querySelector('.style');
     if (link.getAttribute("href") === 'css/colors.css')
     {
         link.href = 'css/other-colors.css';
-        document.querySelector('.presentation').style.background = 'url("../images/background2.jpg")';
     }
     else
     {
         link.href = 'css/colors.css';
-        document.querySelector('.presentation').style.background = 'url("../images/background.jpg")';
     }
 }
